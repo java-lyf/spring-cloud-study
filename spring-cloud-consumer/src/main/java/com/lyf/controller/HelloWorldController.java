82eec8554746c9db25d128a91758f3d0722d61b1
luyufan <761559274@qq.com>
2019-08-25 17:57:02 +0800

    配置

--- spring-cloud-consumer/src/main/java/com/lyf/controller/HelloWorldController.java
+++ spring-cloud-consumer/src/main/java/com/lyf/controller/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @Value("${hello}")
+    @Value("${name}")
     private String name;
 
     @GetMapping("/hello")
