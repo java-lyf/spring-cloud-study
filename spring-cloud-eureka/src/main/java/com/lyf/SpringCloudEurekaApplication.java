b2cef189b8e996a9568a40f858b0c76fcbc84edb
java-lyf <java_lyf@163.com>
2019-08-28 01:07:45 +0800

    更改了文件名

--- spring-cloud-eureka/src/main/java/com/lyf/SpringCloudEurekaApplication.java
+++ spring-cloud-eureka/src/main/java/com/lyf/SpringCloudEurekaApplication.java
@@ -3,15 +3,13 @@ package com.lyf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class SpringCloudErurkeApplication {
+public class SpringCloudEurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudErurkeApplication.class, args);
+        SpringApplication.run(SpringCloudEurekaApplication.class, args);
     }
 
 
--- spring-cloud-eureka/src/test/java/com/lyf/SpringCloudEurekaApplicationTests.java
+++ spring-cloud-eureka/src/test/java/com/lyf/SpringCloudEurekaApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringCloudErurkeApplicationTests {
+public class SpringCloudEurekaApplicationTests {
 
     @Test
     public void contextLoads() {
